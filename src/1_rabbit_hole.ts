5531ec2925de28aeb0ae7a3c66c45421283d8636
Tom <admin@Admins-MacBook-Pro.local>
2023-01-03 19:08:57 +0000

    Remove ts ignore and add Rabbit type

--- src/1_rabbit_hole.ts
+++ src/1_rabbit_hole.ts
@@ -3,7 +3,7 @@ import { meetTheCheshireCat } from './2_cheshire_cat';
 import { clear, print, askQuestion } from '../console';
 
 // NOTE: making a custom type from an array can be very useful
-const holes = ['Badger', 'Fox', 'Swiss Cheese', 'Donut'] as const; // 👉 FIXME ❌
+const holes = ['Badger', 'Fox', 'Swiss Cheese', 'Donut', 'Rabbit'] as const;
 // hover 'Hole' in the below line and see that it's now defined as the union of whatever is in the 'holes' array
 type Hole = typeof holes[number];
 
@@ -35,7 +35,6 @@ export function enterHole(hole: string): void {
 		return endAdventure();
 	}
 
-	// @ts-ignore 👉 FIXME ❌
 	if (holes[number] === 'Rabbit') {
 		return meetTheCheshireCat();
 	} else {
